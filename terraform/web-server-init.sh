eaeb8c0c72714bf2f239d4f13a3aa5741653547f
MalondaClement <46990837+MalondaClement@users.noreply.github.com>
2022-07-04 12:59:30 +0200

    update server init script

--- terraform/web-server-init.sh
+++ terraform/web-server-init.sh
@@ -3,5 +3,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
 
 echo "nginx install done" > /home/debian/log-nginx-install.txt
